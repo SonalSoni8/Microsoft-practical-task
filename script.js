5bb2cb54e6a2e25fc967d905b62942dc0c78f1ce
Sonal Soni <89121450+SonalSoni8@users.noreply.github.com>
2023-10-13 01:34:41 +0530

    design

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
-//DOMContentLoaded dhayan rakhega ke js ka code html ke content jab fully load ho jaye uske baad he js run ho
-document.addEventListener("DOMContentLoaded", function () {
-    const select = document.getElementById("category");
-    const button = document.querySelectorAll(".search-btn");
-    const price = document.getElementById("price-range");
-    const pdisplay = document.getElementById("price-display");
-    const container = document.getElementById("product-container");
-    const prev = document.getElementById("prev-button");
-    const next = document.getElementById("next-button");
-    let products = []; // filter jab krenge toh yaha store ho jayega
-    let currentPage = 0; 
+const select = document.getElementById("category");
+const button = document.querySelectorAll(".search-btn");
+const price = document.getElementById("price-range");
+const pdisplay = document.getElementById("price-display");
+const container = document.getElementById("product-container");
+const prev = document.getElementById("prev-button");
+const next = document.getElementById("next-button");
+let products = []; // filter jab krenge toh yaha store ho jayega
+let currentPage = 0;
 
-    //display products
-    function display() {
-        container.innerHTML = ''; //existing product clear kr dega aur filter product ko aane dega
-        
-        const perPage = 36; //12 column * 3 rows display items per page
-        const startIndex = currentPage * perPage;
-        const endIndex = startIndex + perPage;
+//display products
+function display() {
+    container.innerHTML = ''; //existing product clear kr dega aur filter product ko aane dega
 
-        let pfiltered = products; 
+    const perPage = 36; //12 column * 3 rows display items per page
+    const startIndex = currentPage * perPage;
+    const endIndex = startIndex + perPage;
 
-        //category filter
-        const cselected = select.value;
-        if (cselected !== 'all') {
-            pfiltered = pfiltered.filter(product => product.category === cselected);
+    let pfiltered = products;
+
+    //category filter
+    const cselected = select.value;
+    if (cselected !== 'all') {
+        pfiltered = pfiltered.filter(product => product.category === cselected);
+    }
+
+    //search filters
+    button.forEach(button => {
+        const sfilter = button.getAttribute("data-filter");
+        if (button.classList.contains("active")) {
+            pfiltered = pfiltered.filter(product => sfilter === 'all' || product.searchFor === sfilter);
         }
+    });
 
-        //search filters
-        button.forEach(button => {
-            const sfilter = button.getAttribute("data-filter");
-            if (button.classList.contains("active")) {
-                pfiltered = pfiltered.filter(product => sfilter === 'all' || product.searchFor === sfilter);
-            }
-        });
-
-        //price range filter
-        const pselected = parseFloat(price.value);//so that hume desimal vale na mile we get proper integer
-        pdisplay.textContent = `$${pselected}`;
-        pfiltered = pfiltered.filter(product => product.price <= pselected);
+    //price range filter
+    const pselected = parseFloat(price.value);//so that hume desimal vale na mile we get proper integer
+    pdisplay.textContent = `$${pselected}`;
+    pfiltered = pfiltered.filter(product => product.price <= pselected);
 
 
-        //current page product display
-        const productsDisplay = pfiltered.slice(startIndex, endIndex);
+    //current page product display
+    const productsDisplay = pfiltered.slice(startIndex, endIndex);
 
-        productsDisplay.forEach(product => {
-            const productElement = document.createElement("div");
-            productElement.classList.add("product");
-            productElement.innerHTML = `
+    productsDisplay.forEach(product => {
+        const productElement = document.createElement("div");
+        productElement.classList.add("product");
+        productElement.innerHTML = `
                 <a href="${product.productLink}" target="_blank" class='card'>
                     <img src="${product.image}" alt="${product.name}">
                     <h3>${product.name}</h3>
@@ -54,53 +52,71 @@ document.addEventListener("DOMContentLoaded", function () {
                     <p>Price: $${product.price}</p>
                 </a>
             `;
-            container.appendChild(productElement);
-        });
+        container.appendChild(productElement);
+    });
+}
+
+//fetching data from json
+fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+        products = data; //array me product store ho jayega
+        display();
+    })
+    .catch(error => console.error("Error loading product data:", error));
+
+//for category filter
+select.addEventListener("change", function () {
+    display();
+});
+
+//for "Search For" buttons
+button.forEach(button => {
+    button.addEventListener("click", function () {
+        button.classList.toggle("active");
+        display();
+    });
+});
+
+//for price range filter
+price.addEventListener("input", function () {
+    display();
+});
+
+//for prev button
+prev.addEventListener("click", function () {
+    if (currentPage > 0) {
+        currentPage--;
+        display();
     }
+});
 
-    //fetching data from json
-    fetch("data.json")
-        .then(response => response.json())
-        .then(data => {
-            products = data; //array me product store ho jayega
-            display(); 
-        })
-        .catch(error => console.error("Error loading product data:", error));
-
-    //for category filter
-    select.addEventListener("change", function() {
+//for next button
+next.addEventListener("click", function () {
+    const perPage = 36; //no. of product to be displayed on each page
+    const totalProducts = products.length;
+    const maxPages = Math.ceil(totalProducts / perPage);//math.ceil nearest integer me convert krdega
+    if (currentPage < maxPages - 1) {
+        currentPage++;
         display();
-    });
+    }
+});
 
-    //for "Search For" buttons
+
+const clearbtn = document.getElementById("clear");
+clearbtn.addEventListener("click", function () {
+    // Clear category filter
+    select.value = "all";
+
+    // Clear "Search For" buttons
     button.forEach(button => {
-        button.addEventListener("click", function() {
-            button.classList.toggle("active");
-            display();
-        });
+        button.classList.remove("active");
     });
 
-    //for price range filter
-    price.addEventListener("input", function() {
-        display();
-    });
+    // Reset price range filter
+    price.value = 999;
+    pdisplay.textContent = "$999";
 
-    //for prev button
-    prev.addEventListener("click", function() {
-        if (currentPage > 0) {
-            currentPage--;
-            display();
-        }
-    });
-
-    //for next button
-    next.addEventListener("click", function() {
-        const perPage = 36; //no. of product to be displayed on each page
-        const totalProducts = products.length;
-        const maxPages = Math.ceil(totalProducts / perPage);//math.ceil nearest integer me convert krdega
-        if (currentPage < maxPages - 1) {
-            currentPage++;
-            display();
-        }
-    });
+    // Call the display function to update the product display with cleared filters
+    display();
 });
